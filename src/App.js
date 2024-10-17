b9f52f891b8c35ea2e639c0626a51965bc9c2d59
Mason Nguyen <nhatlinh300401@gmail.com>
2024-10-17 15:10:13 -0700

    Add Route Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
@@ -9,7 +12,12 @@ function App() {
         <Navbar title={"Github Finder"}/>
 
         <main className="container mx-auto px-3 pb-12">
-          Content
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
         </main>
 
         <Footer />
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className='text-6xl'>Welcome to Home Page</h1>
+    </div>
+  )
+}
